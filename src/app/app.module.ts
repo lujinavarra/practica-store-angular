ee39dddf7d99872240ebc9552629ee8bdf788bec
Lujan Navarra <lujinavarra@gmail.com>
2023-06-27 17:28:36 -0300

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,6 +20,7 @@ import { MaterialModule } from './material.module';
     FormsModule,
     MaterialModule,
     BrowserAnimationsModule,
+    HttpClientModule,
 
 
   ],
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
+import { Product } from './interfaces/product.interface';
+import { ProductsService } from './services/products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent {
-
+export class ProductsComponent implements OnInit{
+  products!: Product[]
+  constructor(private productSvc: ProductsService){}
+  ngOnInit(): void {
+    this.productSvc.getProducts()
+    .pipe(
+      tap((products:Product[]) => this.products = products)
+    )
+    .subscribe()
+  }
 }
